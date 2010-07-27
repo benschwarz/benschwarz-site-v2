128005f791f418d1a60d996439ef209fecb2ee5d
Ben Schwarz <ben.schwarz@gmail.com>
2010-07-27 21:59:00 +1000

    Remove Jammit rendering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
-  
-  before_filter { Jammit.packager.precache_all } if Rails.env.development?
 end
