01a734f84537f76cd68301fe82b66c81719d2744
Ben Schwarz <ben.schwarz@gmail.com>
2010-07-28 11:07:28 +1000

    Added details to bio screen

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def entity_encode(string)
+    output_array = []
+    lower = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+    upper = %w(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+    char_array = string.split('')
+    char_array.each do |char|  
+      output = lower.index(char) + 97 if lower.include?(char)
+      output = upper.index(char) + 65 if upper.include?(char)
+      if output
+        output_array << "&##{output};"
+      else 
+        output_array << char
+      end
+    end
+    return output_array.join
+  end
 end
