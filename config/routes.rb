301b54edd2fe012c40905a6828fb9ed6a40ae9d6
Ben Schwarz <ben.schwarz@gmail.com>
2010-10-08 21:43:28 +1100

    Redirect /articles to root

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ BenschwarzSite::Application.routes.draw do |map|
   root :to => "pages#about"
 
   # match "/about", :to => "pages#about"
+  # redirect /articles to index
   resources :articles, :only => :show
+  match "/articles", :to => redirect("/")
 end
