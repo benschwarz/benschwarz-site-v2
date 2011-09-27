3091265c3b279b9b3bed86e33b6b57aabbd000e4
Ben Schwarz <ben.schwarz@gmail.com>
2011-09-27 21:15:55 +1000

    Use pinboard for links. Bye delicious.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < HighVoltage::PagesController
   def about
     @articles = Article.all.sort
-    rc = RestClient.get("http://feeds.delicious.com/v2/rss/bschwarz/my-site+presentation")
-    @presentations = Transcoder.for(rc.headers[:content_type]).parse(rc.to_s).entries
+    rc = RestClient.get("http://feeds.pinboard.in/rss/secret:f36ba76866d89cc644b0/u:benschwarz/t:presentation/t:my-site")
+    @presentations = Transcoder.for(rc.headers[:content_type]).parse(rc.to_s).first[1]['item']
   end
 end
