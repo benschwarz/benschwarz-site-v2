f56e3166a00722ffc414a544e42a195a05d98773
Ben Schwarz <ben.schwarz@gmail.com>
2011-12-10 21:27:52 +1100

    Fix my broken site. Gee. Thanks pinboard.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < HighVoltage::PagesController
   def about
     @articles = Article.all.sort
-    rc = RestClient.get("http://feeds.pinboard.in/rss/secret:f36ba76866d89cc644b0/u:benschwarz/t:presentation/t:my-site")
-    @presentations = Transcoder.for(rc.headers[:content_type]).parse(rc.to_s).first[1]['item']
+    
+    begin
+      rc = RestClient.get("http://feeds.pinboard.in/rss/secret:f36ba76866d89cc644b0/u:benschwarz/t:presentation/t:my-site")
+      @presentations = Transcoder.for(rc.headers[:content_type]).parse(rc.to_s).first[1]['item']
+    rescue 
+      @presentations = []
+    end
   end
 end
